b5d39d23d1efa23005e12bdd636cc0db267b7976
Michael Del Pape <m4delpape@gmail.com>
2023-09-30 15:53:58 -0700

    deploying

--- src/components/Plane.jsx
+++ src/components/Plane.jsx
@@ -11,7 +11,7 @@ import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 
 export default function Plane(props) {
-  const { nodes, materials } = useGLTF('/public/assets/uploads_files_4140769_plane-transformed.glb')
+  const { nodes, materials } = useGLTF('/assets/uploads_files_4140769_plane-transformed.glb')
   const objectRef = useRef();
 
   const orbitRadius = 20;
